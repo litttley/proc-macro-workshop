20316e0dff0251ebb041624eb03d208ebdf89f55
litttley <617914118@qq.com>
2020-07-07 22:49:58 +0800

    学习注释

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -9,6 +9,7 @@ use syn::{parse_macro_input, DeriveInput};
 #[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
+    println!("{:#?}",ast);
     let name = &ast.ident;
     let bname = format!("{}Builder", name);
     let bident = syn::Ident::new(&bname, name.span());
